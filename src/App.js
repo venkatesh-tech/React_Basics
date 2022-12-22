f6f8958dd6e225b5ed633b54246342cccfe446af
Venkateshe <kolapallinagavenkatesh@gmail.com>
2022-12-22 10:14:06 +0530

    Added JSX

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from "./logo.svg";
 import Greet from "./components/Greet";
 import Welcome from "./components/Welcome";
+import Hello from "./components/Hello";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Greet />
-      <Welcome />
+      {/* <Greet />
+      <Welcome /> */}
+      <Hello />
     </div>
   );
 }
---
+++ src/components/Hello.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+// function hello() {
+//   return <h1>Hello World</h1>;
+// }
+
+const Hello = () => {
+  //   return (
+  //     <div className ="dummyName">
+  //       <h1>Hello Venkatesh</h1>   // using JSX
+  //     </div>
+  //   );
+};
+
+export default Hello;
