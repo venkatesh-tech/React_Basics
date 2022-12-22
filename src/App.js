6d0b6d40c740d867603e23c2f5912cee427bd545
Venkateshe <kolapallinagavenkatesh@gmail.com>
2022-12-22 18:18:40 +0530

    Methods as props

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Counter from "./components/Counter";
 import FunctionClick from "./components/FunctionClick";
 import ClassClick from "./components/ClassClick";
 import EventBind from "./components/EventBind";
+import ParentComponent from "./components/ParentComponent";
 
 // function App() {
 //   return (
@@ -41,7 +42,8 @@ class App extends Component {
         {/* <FunctionClick /> */}
         {/* <ClassClick /> */}
         {/* <PracticeFunction name="Bruce" /> */}
-        <EventBind />
+        {/* <EventBind /> */}
+        <ParentComponent />
       </div>
     );
   }
