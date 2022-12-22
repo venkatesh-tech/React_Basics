b4ef5deab4b3fc1b0b2b5e2737e3948e72193108
Venkateshe <kolapallinagavenkatesh@gmail.com>
2022-12-22 12:58:53 +0530

    Added setState-2

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -12,22 +12,34 @@ class Counter extends Component {
     // this.state.count = this.state.count + 1; // for this value is updated in console but not changed in ui
     // console.log(this.state.count);
 
-    this.setState(
-      {
-        count: this.state.count + 1,
-      },
-      () => {
-        console.log("Call Back Value", this.state.count);
-      }
-    );
-    console.log(this.state.count); // Asynchronous and synchronous
+    //     this.setState(
+    //       {
+    //         count: this.state.count + 1,
+    //       },
+    //       () => {
+    //         console.log("Call Back Value", this.state.count);
+    //       }
+    //     );
+    //     console.log(this.state.count); // Asynchronous and synchronous
+
+    this.setState((prevState) => ({
+      count: prevState.count + 1,
+    }));
+  }
+
+  incrementFive() {
+    this.increment();
+    this.increment();
+    this.increment();
+    this.increment();
+    this.increment();
   }
 
   render() {
     return (
       <div>
         Count - {this.state.count}
-        <button onClick={() => this.increment()}>Incremet</button>
+        <button onClick={() => this.incrementFive()}>Incremet</button>
       </div>
     );
   }
