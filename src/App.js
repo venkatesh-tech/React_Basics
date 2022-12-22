77755cd8be87950876c8106552c61ebbba6d915e
Venkateshe <kolapallinagavenkatesh@gmail.com>
2022-12-22 11:06:26 +0530

    Added props

--- src/App.js
+++ src/App.js
@@ -3,15 +3,35 @@ import Greet from "./components/Greet";
 import Welcome from "./components/Welcome";
 import Hello from "./components/Hello";
 import "./App.css";
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      {/* <Greet />
-      <Welcome /> */}
-      <Hello />
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <Greet name="Bruce" heroName="Batman">
+//         <p>This is children props</p>
+//       </Greet>
+//       <Greet name="clark" heroName="Superman">
+//         <button>button</button>
+//       </Greet>
+
+//       <Greet name="Diana" heroName="WonderWoman" />
+//       {/* <Welcome />
+//       <Hello /> */}
+//     </div>
+//   );
+// }
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Greet name="Bruce" heroName="Batman">
+          <p>This is children props</p>
+        </Greet>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Greet.js
+++ src/components/Greet.js
@@ -1,5 +1,27 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Greet = () => <h1>Hello World</h1>;
+// const Greet = (props) => {
+//   console.log(props);
+//   return (
+//     <React.Fragment>
+//       <div>
+//         <h1>Hello {props.name} </h1>
+//         <h1>
+//           {props.heroName} {props.children}
+//         </h1>
+//       </div>
+//     </React.Fragment>
+//   );
+// };
+
+class Greet extends Component {
+  render() {
+    return (
+      <div>
+        <h1>{this.props.name}</h1>
+      </div>
+    );
+  }
+}
 
 export default Greet;
